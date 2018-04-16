0f6fbdd5a77ae0fcd5c95a6024993704dfd853a8
JonasTheGamer <38383712+JonasTheGamer@users.noreply.github.com>
2018-04-16 06:47:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,24 +1,102 @@
-         
+ // setup        
 var prefix = '!';
-const Discord = require('discord.js');
-const client = new Discord.Client();
-          
+var discord = require('discord.js');
+var roblox = require('roblox-js');
+var request = require("request");
+var client = new discord.Client();
+
+//Login into roblox, but not needed for this bot
+roblox.login({username: "marketmanager1", password: "Digitaalsloefje20Nummerke5"}).then((success) => {
+}).catch(() => {console.log("Sorry, it failed.");}); 
+
 function isCommand(command, message){
 	var command = command.toLowerCase();
 	var content = message.content.toLowerCase();
 	return content.startsWith(prefix + command);
 }
-client.on('ready', () => {
-  client.user.setGame('GAME HERE')
-})
-console.log("I'm here!")
+
+var started = true
 client.on('message', (message) => {
-	console.log(message.content)
 	if (message.author.bot) return; // Dont answer yourself.
-	var args = message.content.split(/[ ]+/)
+    var args = message.content.split(/[ ]+/)
     
-	if(isCommand('Ping', message)){
-		message.reply('LOL');
-	}
+    if (isCommand('stopbot',message)){
+        message.channel.send('Bye!')
+        process.exit()
+    }
+    if (isCommand('getmessages',message)){
+        roblox.getMessages(1, 1).then(function (inbox) {
+            message.reply(inbox.messages[0]);
+        });
+    }
+    if (isCommand('cp',message)){
+        var ppname = args[1]
+        var reply = "not found"
+        
+        var st1 = "https://api.roblox.com/users/get-by-username?username="
+        var ul =  st1.concat(ppname)
+        request(ul, function(error, response, body) {
+            if (body == '{"success":false,"errorMessage":"User not found"}') {
+                message.channel.send("User not found!")
+            }else{
+                var st1 = "Here are the gamepasses for "
+                var reply = st1.concat(ppname)
+                message.channel.send(reply)
+                var arr = JSON.parse(body);
+                var Id = arr.Id;
+
+                // SUITE LANE CHECK
+
+                var st1 = "https://api.roblox.com/Ownership/HasAsset?userId="
+                var st2 = "&assetId=414378603"
+                var rmsg = st1.concat(Id)
+                rmsg  = rmsg.concat(st2)
+                request(rmsg, function(error, response, body) {
+                    if (body == "true"){
+                        var t1 = "Suite gamepass:     "
+                        var rep = t1.concat(" :white_check_mark:  ")
+                        message.channel.send(rep)
+                    } else {
+                        var t1 = "Suite gamepass:     "
+                        var rep = t1.concat(" :x:  ")
+                        message.channel.send(rep) 
+                    }
+                });
+
+                var st1 = "https://api.roblox.com/Ownership/HasAsset?userId="
+                var st2 = "&assetId=414376392"
+                var rmsg = st1.concat(Id)
+                rmsg  = rmsg.concat(st2)
+                //FAST LANE CHECK 
+
+                request(rmsg, function(error, response, body) {
+                    if (body == "true"){
+                        var t1 = "Fast Lane gamepass: "
+                        var rep = t1.concat(" :white_check_mark:  ")
+                        message.channel.send(rep)
+                    } else {
+                        var t1 = "Fast Lane gamepass: "
+                        var rep = t1.concat(" :x:  ")
+                        message.channel.send(rep) 
+                    }
+                });
+            }
+
+        });
+        
+        
+    }
+    if (isCommand('getlastpost',message)){
+        var groupid = args[1]
+        roblox.getWall(groupid)
+        .then(function(wall){
+            message.reply(wall.posts.post[1].content)
+            
+        })
+    }
+
+    
+    
 });
+
 client.login(process.env.BOT_TOKEN);
