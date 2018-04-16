add92f0d45a7a4c50da1c31ee6d28b2e8c8a7a16
JonasTheGamer <38383712+JonasTheGamer@users.noreply.github.com>
2018-04-16 07:36:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,48 +18,42 @@ function isCommand(command, message){
 var started = true
 client.on('message', (message) => {
 	if (message.author.bot) return; // Dont answer yourself.
-    var args = message.content.split(/[ ]+/)
-    
-    if (isCommand('stopbot',message)){
-        message.channel.send('Bye!')
-        process.exit()
-    }
-    if (isCommand('getmessages',message)){
-        roblox.getMessages(1, 1).then(function (inbox) {
-            message.reply(inbox.messages[0]);
-        });
-    }
-    if (isCommand('cp',message)){
-        var ppname = args[1]
-        var reply = "not found"
+   	 var args = message.content.split(/[ ]+/)
+   	 if (message.channel.isPrivate) {
+		message.channel.reply("Just use the server please, you don't have to DM me. :smile:")
+   	 } 
+
+   	 if (isCommand('cp',message)){
+		var ppname = args[1]
+    	   	var reply = "not found"
         
-        var st1 = "https://api.roblox.com/users/get-by-username?username="
-        var ul =  st1.concat(ppname)
-        request(ul, function(error, response, body) {
-            if (body == '{"success":false,"errorMessage":"User not found"}') {
-                message.channel.send("User not found!")
-            }else{
-                var st1 = "Here are the gamepasses for "
-                var reply = st1.concat(ppname)
-                message.channel.send(reply)
-                var arr = JSON.parse(body);
-                var Id = arr.Id;
+    	   	var st1 = "https://api.roblox.com/users/get-by-username?username="
+      	 	var ul =  st1.concat(ppname)
+       		request(ul, function(error, response, body) {
+           	if (body == '{"success":false,"errorMessage":"User not found"}') {
+                	message.channel.send("User not found!")
+            	}else{
+                	var st1 = "Here are the gamepasses for "
+                	var reply = st1.concat(ppname)
+                	message.channel.send(reply)
+                	var arr = JSON.parse(body);
+                	var Id = arr.Id;
 
-                // SUITE LANE CHECK
+			// SUITE LANE CHECK
 
-                var st1 = "https://api.roblox.com/Ownership/HasAsset?userId="
-                var st2 = "&assetId=414378603"
-                var rmsg = st1.concat(Id)
-                rmsg  = rmsg.concat(st2)
-                request(rmsg, function(error, response, body) {
-                    if (body == "true"){
-                        var t1 = "Suite gamepass:     "
-                        var rep = t1.concat(" :white_check_mark:  ")
-                        message.channel.send(rep)
-                    } else {
-                        var t1 = "Suite gamepass:     "
-                        var rep = t1.concat(" :x:  ")
-                        message.channel.send(rep) 
+			var st1 = "https://api.roblox.com/Ownership/HasAsset?userId="
+			var st2 = "&assetId=414378603"
+			var rmsg = st1.concat(Id)
+			rmsg  = rmsg.concat(st2)
+			request(rmsg, function(error, response, body) {
+                    	if (body == "true"){
+                      		var t1 = "Suite gamepass:     "
+                      	  	var rep = t1.concat(" :white_check_mark:  ")
+                        	message.channel.send(rep)
+                    	} else {
+                        	var t1 = "Suite gamepass:     "
+                        	var rep = t1.concat(" :x:  ")
+                        	message.channel.send(rep) 
                     }
                 });
 
