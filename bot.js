7ca800aacdd6cead618d7e23daf5e731b3a5e71e
JonasTheGamer <38383712+JonasTheGamer@users.noreply.github.com>
2018-04-20 14:57:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,6 @@ function isCommand(command, message){
 
 var started = true
 client.on('message', (message) => {
-	console.log(message.channel.type);
 	if (message.author.bot) return; // Dont answer yourself.
    	 var args = message.content.split(/[ ]+/)
    	 if (message.channel.type == "dm") {
